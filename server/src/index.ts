b3b47a8ae54db297e073e6ad9412fe941c13ea3c
지오현 <jihkyuoo@naver.com>
2023-10-03 02:19:56 +0900

    client server init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
-export const app = express();
+const app = express();
+
+const logger = morgan('dev');
+
+app.use(logger);
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`✅ Server Listening on port http://localhost:${PORT}`));
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './client/dist',
+  },
   resolve: {
     alias: [
       { find: /^~/, replacement: '' },
-      { find: /^@(?=\/)/, replacement: path.resolve(__dirname, './src') },
+      { find: /^c@(?=\/)/, replacement: path.resolve(__dirname, './client/src') },
     ],
   },
-  plugins: [react()],
-})
+  plugins: [ react() ],
+});

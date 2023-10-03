76f0770157dd07163576baa58c4a9939a1165605
지오현 <jihkyuoo@naver.com>
2023-10-03 11:16:09 +0900

    cors 처리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css';
+import { Api } from 'c@/common/service/api.service';
+import { useEffect } from 'react';
+
+
+const testApi = async () => {
+  return await Api.get(`/`);
+};
 
 function App() {
 
+  useEffect(() => {
+    testApi().then(console.log);
+  }, []);
+
   return (
     <>
+      test
     </>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+import { CorsOptions } from 'vite';
 
 const app = express();
 
 const logger = morgan('dev');
+const corsOptions: CorsOptions = {
+  origin: 'http://localhost:3003',
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
 app.use(logger);
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => res.send('Hello World'));
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`✅ Server Listening on port http://localhost:${PORT}`));
